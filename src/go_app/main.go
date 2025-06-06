bf11ef4b2b07c3894cf5f9f427a899187428059b
Vung Do <vungxd04@gmail.com>
2025-06-06 23:14:48 +0700

    first sample trace with go app

--- src/go_app/main.go
+++ src/go_app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"io"
 	"log"
@@ -9,6 +10,13 @@ import (
 
 	"go_app/hello" // Replace with the actual path of generated code
 
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
+	"google.golang.org/grpc"
 	"google.golang.org/protobuf/proto"
 )
 
@@ -33,7 +41,41 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/hello", helloHandler)
+	ctx := context.Background()
+
+	// Initialize OTLP exporter over gRPC (default endpoint: localhost:4317)
+	exporter, err := otlptracegrpc.New(ctx,
+		otlptracegrpc.WithInsecure(), // or use WithTLSCredentials
+		otlptracegrpc.WithEndpoint("otel-collector:4317"),
+		otlptracegrpc.WithDialOption(grpc.WithBlock()),
+	)
+
+	if err != nil {
+		log.Fatalf("failed to create OTLP exporter: %v", err)
+	}
+
+	res, err := resource.New(ctx,
+		resource.WithAttributes(
+			semconv.ServiceName("go_app"),
+		),
+	)
+	if err != nil {
+		log.Fatalf("Failed to create resource: %v", err)
+	}
+
+	tp := sdktrace.NewTracerProvider(
+		sdktrace.WithBatcher(exporter),
+		sdktrace.WithResource(res),
+	)
+	defer func() {
+		_ = tp.Shutdown(ctx)
+	}()
+
+	otel.SetTracerProvider(tp)
+
+	// Wrap handler with otelhttp middleware
+	http.Handle("/hello", otelhttp.NewHandler(http.HandlerFunc(helloHandler), "HelloHandler"))
+
 	fmt.Println("Go Protobuf server listening on :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
