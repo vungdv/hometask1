19a06464323794b6b0461edc44a6e1d35d477a12
Vung Do <vungxd04@gmail.com>
2025-07-03 17:19:23 +0700

    Move endpoint to config

--- src/app/Features/Hello/Endpoints/HelloEndpoints.cs
+++ src/app/Features/Hello/Endpoints/HelloEndpoints.cs
@@ -9,6 +9,7 @@ namespace app.Features.Weather.Endpoints;
 
 public static class HelloEndpoints
 {
+    private const string GOAPPServiceEndpointConfig = "go_app:endpoint";
     static readonly Action<ILogger, string, Exception?> _preCompiledLogMessage =
     LoggerMessage.Define<string>(
         logLevel: LogLevel.Information,
@@ -17,9 +18,9 @@ public static class HelloEndpoints
 
     public static void MapHelloEndpoints(this WebApplication app)
     {
-        app.MapGet("/hello", async (IHttpClientFactory httpClientFactory, ILogger<Program> logger) =>
+        app.MapGet("/hello", async (IHttpClientFactory httpClientFactory, IConfiguration config, ILogger<Program> logger) =>
         {
-            var reply = await HelloProtobufAsync(httpClientFactory, logger);
+            var reply = await HelloProtobufAsync(httpClientFactory, config, logger);
             Meters.HelloCount.Add(1);
             return new { reply.Message };
         })
@@ -27,8 +28,11 @@ public static class HelloEndpoints
         .WithOpenApi();
     }
 
-    static async Task<HelloReply> HelloProtobufAsync(IHttpClientFactory httpClientFactory, ILogger logger)
+    static async Task<HelloReply> HelloProtobufAsync(IHttpClientFactory httpClientFactory, IConfiguration config, ILogger logger)
     {
+        var goappEndpoint = config.GetValue<string>(GOAPPServiceEndpointConfig);
+        ArgumentNullException.ThrowIfNull(goappEndpoint);
+
         using var activity = Tracing.ServiceActivitySource.StartActivity("HelloActivity", ActivityKind.Client);
 
         if (Activity.Current != null)
@@ -44,12 +48,15 @@ public static class HelloEndpoints
         Baggage.SetBaggage("user_id", "123456");
 
         var client = httpClientFactory.CreateClient();
+
+        client.BaseAddress = new Uri(uriString: goappEndpoint);
+
         var request = new HelloRequest { Name = "Alice2" };
         _preCompiledLogMessage(logger, request.Name, null);
         var content = new ByteArrayContent(request.ToByteArray());
         content.Headers.ContentType = new MediaTypeHeaderValue("application/x-protobuf");
 
-        var response = await client.PostAsync("http://go_app:8080/hello", content);
+        var response = await client.PostAsync("/hello", content);
         var respBytes = await response.Content.ReadAsByteArrayAsync();
 
         return HelloReply.Parser.ParseFrom(respBytes);
