d3277f3408df8163112a9c829278aa0a8fbc6f65
Vung Do <vungxd04@gmail.com>
2025-07-04 17:22:39 +0700

    update k6 test

--- tests/k6/hello_products.js
+++ tests/k6/hello_products.js
@@ -4,12 +4,12 @@ import { check, sleep } from "k6";
 const BASE_URL = __ENV.GO_APP_BaseUrl;
 
 export const options = {
-  vus: 100, // 10 virtual users
+  vus: 10, // 10 virtual users
   duration: "30s", // test runs for 30 seconds
 };
 
 export default function () {
-  const res = http.get(`${BASE_URL}/products/?skip=19&limit=15`);
+  const res = http.get(`${BASE_URL}/products/?skip=10&limit=10`);
 
   // Parse JSON response
   const data = res.json();
@@ -17,8 +17,7 @@ export default function () {
   check(res, {
     "status is 200": (r) => r.status === 200,
     "contains products": () => Array.isArray(data.products),
-    "has at least 1 product": () => data.products.length == 15,
-    "first product has ID": () => data.products[0]?.id === 30,
+    "has at least 1 product": () => data.products.length == 10,
     "first product has title": () =>
       typeof data.products[0]?.title === "string",
   });
--- tests/k6/hello_test.js
+++ tests/k6/hello_test.js
@@ -4,8 +4,8 @@ import { check, sleep } from "k6";
 const BASE_URL = __ENV.APP_BaseUrl;
 
 export const options = {
-  vus: 100, // 10 virtual users
-  duration: "30s", // test runs for 30 seconds
+  vus: 1, // 10 virtual users
+  duration: "1s", // test runs for 30 seconds
 };
 
 export default function () {
