8098548d962b4f936a6dff8a685626bcafd70faa
vung.do <vungxd04@gmail.com>
2026-08-29 16:37:45 +0700

    Init spring web

---
+++ src/java/03-Polaris/src/main/java/vn/danang/Polaris/Controllers/HelloController.java
@@ -0,0 +1,14 @@
+package vn.danang.Polaris.Controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    
+    @GetMapping("/api/v1/hello")
+    public String hello(){
+        return "Hello, world!";
+    }
+
+}
---
+++ src/java/03-Polaris/src/main/java/vn/danang/Polaris/PolarisApp.java
@@ -0,0 +1,11 @@
+package vn.danang.Polaris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolarisApp {
+    public static void main(String[] args) {
+        SpringApplication.run(PolarisApp.class, args);
+    }
+}
